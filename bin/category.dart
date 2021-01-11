64a3769ab8d6ebb842b095de59cadf7f744f505e
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-11 21:15:10 +0000

    Create exercise category data model

---
+++ bin/category.dart
@@ -0,0 +1,35 @@
+// External Imports
+import 'dart:convert' show jsonEncode, jsonDecode;
+import 'package:meta/meta.dart' show required;
+
+/// Encapsulates all the data associated to a category, as described in the
+/// database
+///
+/// ```sql
+/// `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+/// `name` VARCHAR(64) UNIQUE NOT NULL
+/// ```
+class Category {
+  int id;
+  String name;
+
+  /// Constructor from 'raw data'
+  Category({
+    @required this.id,
+    @required this.name,
+  });
+
+  Category.fromJson(final String json) : this.fromMap(jsonDecode(json));
+  Category.fromMap(final Map<String, dynamic> map) {
+    id = int.parse(map['category_id']);
+    name = map['category_name'];
+  }
+
+  String toJson() => jsonEncode(toMap());
+  Map<String, dynamic> toMap() {
+    return {
+      'category_id': id,
+      'category_name': name,
+    };
+  }
+}
