96ca1ef383e72037e31ea85c04487c889321bee5
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-11 21:15:35 +0000

    Create workout log access level data model

---
+++ bin/access_level.dart
@@ -0,0 +1,35 @@
+// External Imports
+import 'dart:convert' show jsonEncode, jsonDecode;
+import 'package:meta/meta.dart' show required;
+
+/// Encapsulates all the data associated to a workout access level, as described
+/// in the database
+///
+/// ```sql
+/// `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+/// `name` VARCHAR(64) UNIQUE NOT NULL
+/// ```
+class AccessLevel {
+  int id;
+  String name;
+
+  /// Constructor from 'raw data'
+  AccessLevel({
+    @required this.id,
+    @required this.name,
+  });
+
+  AccessLevel.fromJson(final String json) : this.fromMap(jsonDecode(json));
+  AccessLevel.fromMap(final Map<String, dynamic> map) {
+    id = int.parse(map['access_level_id']);
+    name = map['access_level_name'];
+  }
+
+  String toJson() => jsonEncode(toMap());
+  Map<String, dynamic> toMap() {
+    return {
+      'access_level_id': id,
+      'access_level_name': name,
+    };
+  }
+}
