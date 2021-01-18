88179f5c4b30c746b567bd276baf006192b790b8
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-18 20:49:50 +0000

    Create ew_map data model

---
+++ ew_map/ew_map.dart
@@ -0,0 +1,42 @@
+// External imports
+import 'dart:convert' show jsonDecode;
+
+// Local imports
+import '../insertable.dart';
+import '../jsonable.dart';
+import 'table.dart';
+
+class EwMap extends Jsonable with Insertable {
+  int exerciseId;
+  int workoutId;
+  int sets;
+
+  EwMap({this.exerciseId, this.workoutId, this.sets});
+  EwMap.fromJson(final String json) : this.fromMap(jsonDecode(json));
+  EwMap.fromMap(final Map<String, dynamic> map) {
+    exerciseId = int.parse(Jsonable.tryExtract(map, Table.exercise, '0'));
+    workoutId = int.parse(Jsonable.tryExtract(map, Table.workout, '0'));
+    sets = int.parse(Jsonable.tryExtract(map, Table.sets, '0'));
+  }
+
+  @override
+  bool hasDataForInsert() => hasDataForUpdate();
+
+  @override
+  bool hasDataForUpdate() => exerciseId > 0 && workoutId > 0 && sets > 0;
+
+  @override
+  List toInsertArray() => [exerciseId, workoutId, sets];
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      Table.exercise: exerciseId,
+      Table.workout: workoutId,
+      Table.sets: sets,
+    };
+  }
+
+  @override
+  List toUpdateArray() => [sets, exerciseId, workoutId];
+}
