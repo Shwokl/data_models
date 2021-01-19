0167cc9e1e7079911f2d87fe3e5e2a35fefd2027
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-19 11:36:21 +0000

    Add *_map classes to the factories map

--- jsonable.dart
+++ jsonable.dart
@@ -3,11 +3,14 @@ import 'dart:convert' show jsonEncode;
 
 // Local imports
 import 'categories/category.dart';
+import 'emg_map/emg_map.dart';
+import 'ew_map/ew_map.dart';
 import 'exercises/exercise.dart';
 import 'log_entries/log_entry.dart';
 import 'muscle_groups/muscle_group.dart';
 import 'roles/role.dart';
 import 'users/user.dart';
+import 'uwp_map/uwp_map.dart';
 import 'workouts/workout.dart';
 import 'workout_logs/workout_log.dart';
 import 'workout_plans/workout_plan.dart';
@@ -24,6 +27,9 @@ abstract class Jsonable {
     WorkoutLog: (String json) => WorkoutLog.fromJson(json),
     WorkoutPlan: (String json) => WorkoutPlan.fromJson(json),
     Workout: (String json) => Workout.fromJson(json),
+    EmgMap: (String json) => EmgMap.fromJson(json),
+    EwMap: (String json) => EwMap.fromJson(json),
+    UwpMap: (String json) => UwpMap.fromJson(json),
   };
 
   static T fromJson<T extends Jsonable>(final String json) {
